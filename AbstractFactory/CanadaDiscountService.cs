6a24ee7574cfa69279b9c01da7ea2d34e1d05178
Cullen Prunty <cprunty@greatland.com>
2022-08-26 08:35:16 -0500

    add abstract factory code using vs2022

---
+++ AbstractFactory/CanadaDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class CanadaDiscountService : IDiscountService
+    {
+        public int DiscountPercentage => 20;
+    }
+}
---
+++ AbstractFactory/FranceShoppingCartPurchaseFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class FranceShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new FranceDiscountService();
+        }
+
+        public IShippingCostsService CreateShippingCostsService()
+        {
+            return new FranceShippingCostsService();
+        }
+    }
+}
